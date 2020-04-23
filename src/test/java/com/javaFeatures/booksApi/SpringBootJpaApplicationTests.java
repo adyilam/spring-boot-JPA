23f40a87d0eb25f814ee2020e6fcd227812c99ac
Ade <amare.aderajew@gmail.com>
2020-04-23 15:57:40 -0400

    books api first change

---
+++ src/test/java/com/javaFeatures/booksApi/SpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaFeatures.booksApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
