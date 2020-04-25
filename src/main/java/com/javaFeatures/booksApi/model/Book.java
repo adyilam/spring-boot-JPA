e47421e45c9eebc2d5712323858e0c3c9a85ad37
Ade <amare.aderajew@gmail.com>
2020-04-25 17:03:31 -0400

    wip

--- src/main/java/com/javaFeatures/booksApi/model/Book.java
+++ src/main/java/com/javaFeatures/booksApi/model/Book.java
@@ -1,10 +1,13 @@
 package com.javaFeatures.booksApi.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.Id;
 
 import javax.persistence.Entity;
 
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Book {
     @Id
     private String id;
